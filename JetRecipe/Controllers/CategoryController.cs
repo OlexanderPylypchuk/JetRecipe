67ba95a47203231219e2fabaf9c990900cc02da0
OleksanderPylypchuk <o.s.pylypchuk@student.khai.edu>
2024-08-23 15:47:59 +0300

    CategoryController

--- JetRecipe/Controllers/CategoryController.cs
+++ JetRecipe/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using JetRecipe.Api.Data;
+using JetRecipe.Api.Models;
 using JetRecipe.Api.Models.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +36,80 @@ namespace JetRecipe.Api.Controllers
 				return _responceDto;
 			}
 			
+		}
+		[HttpGet("{id:int}")]
+		public async Task<ResponceDto> Get(int id)
+		{
+			try
+			{
+				var item = await _db.Categories.FirstOrDefaultAsync(c=>c.Id==id);
+				_responceDto.Success = true;
+				_responceDto.Result = item;
+				return _responceDto;
+			}
+			catch (Exception ex)
+			{
+				_responceDto.Success = false;
+				_responceDto.Message = ex.Message;
+				return _responceDto;
+			}
+
+		}
+		[HttpPost]
+		[Authorize(Roles = "admin")]
+		public async Task<ResponceDto> Create([FromBody]Category category)
+		{
+			try
+			{
+				await _db.Categories.AddAsync(category);
+				await _db.SaveChangesAsync();
+				_responceDto.Success = true;
+				_responceDto.Result = category;
+			}
+			catch (Exception ex)
+			{
+				_responceDto.Success = false;
+				_responceDto.Message = ex.Message;
+			}
+			return _responceDto;
+		}
+		[HttpPut]
+		[Authorize(Roles = "admin")]
+		public async Task<ResponceDto> Update([FromBody] Category category)
+		{
+			try
+			{
+				_db.Categories.Update(category);
+				await _db.SaveChangesAsync();
+				_responceDto.Success = true;
+				_responceDto.Result = category;
+			}
+			catch (Exception ex)
+			{
+				_responceDto.Success = false;
+				_responceDto.Message = ex.Message;
+			}
+			return _responceDto;
+		}
+		[HttpDelete]
+		[Authorize(Roles = "admin")]
+		[Route("/api/recipe/{id:int}")]
+		public async Task<ResponceDto> Delete(int id)
+		{
+			try
+			{
+				var category = await _db.Categories.FindAsync(id);
+				_db.Categories.Remove(category);
+				await _db.SaveChangesAsync();
+				_responceDto.Success = true;
+				_responceDto.Result = category;
+			}
+			catch (Exception ex)
+			{
+				_responceDto.Success = false;
+				_responceDto.Message = ex.Message;
+			}
+			return _responceDto;
 		}
 	}
 }
